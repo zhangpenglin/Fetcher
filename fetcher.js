225ef3c3731c3a9ce27eabd083fd1fda3779d87a
Zhang Penglin <88379382@qq.com>
2016-07-01 16:12:21 +0800

    new

--- fetcher.js
+++ fetcher.js
@@ -16,7 +16,7 @@ class Fetcher {
     fetchData() {
         this.log('正在抓取数据...')
 
-        Promise.all(this.params.reqs).then(r=> {
+       return Promise.all(this.params.reqs).then(r=> {
             this.log('正在解析数据...')
 
             return r.map(this.params.parseData).reduce((previous, item)=> previous.concat(item))
@@ -26,50 +26,16 @@ class Fetcher {
                 this.writeData(data)
                 this.log('发现新数据')
                 this.log(newData)
-                return this.sendMessage(newData)
+                return newData
             }
             this.log('未发现新数据')
-            return 'noNewData'
-        }).then(data=> {
-            this.delayFetch()
-        }).catch(data=> {
-            this.log('发生错误')
-            this.log(data)
-            this.delayFetch()
-        })
+            return []
+        }).catch(()=>{
+           return []
+       })
     }
 
-    delayFetch() {
-        const currentTime = new Date()
-        if (currentTime.getHours() == config.stopHour) {
-            //下班时间，延迟到明天上班再抓取
-            const timeout = this.getNextFetchTimeout()
-            this.log(timeout + '分钟后将进行下次抓取')
-            setTimeout(this.fetchData.bind(this), timeout * 60 * 1000)
-        } else {
-            this.log(config.timeout + '分钟后将进行下次抓取')
-            setTimeout(this.fetchData.bind(this), 20 * 1000)
-        }
-    }
 
-    getNextFetchTimeout() {
-        const hour = (24 - config.stopHour) + config.startHour
-        return hour * 60
-    }
-
-    sendMessage(data) {
-        // const msg = data.map(d=>`[${d.title}|${d.url}]`)
-        // config.messageQuery.qs.msg = msg.join(' ')
-        // this.log(config.messageQuery)
-        return request({url: "http://127.0.0.1:8012/sendnotify.cgi"
-            ,form:{
-                msg:"sdf阿斯蒂芬",
-                receiver:"123"
-            },
-        proxy:"http://127.0.0.1:8888"
-
-        })
-    }
 
     checkUpdateData(dataToCheck) {
         this.log('正在比对数据...')
--- index.js
+++ index.js
@@ -3,10 +3,20 @@ const cheerio = require('cheerio');
 const Promise = require('bluebird');
 const request = Promise.promisify(require('request'));
 const fs = require("fs");
+const exec = require("child_process").exec
+const config = require('./config.json');
 
+exec('anywhere 3333 -s')
 
+function sendMessage(data) {
+    const config=JSON.parse(fs.readFileSync('./config.json', 'utf-8'))
+    const msg = config.messageQuery.qs.msg
+    const now = Date.now()
+    config.messageQuery.qs.msg = (new Date()).toLocaleString()+"  "+msg + 'Click [ here | ' + config.serverPort + '/index.html?ver=' + now + '] to get more info'
+    return request(config.messageQuery)
+}
 function gethebgc() {
-    const taskName='hebgc'
+    const taskName = 'hebgc'
     const preUrl = 'http://www.hebgc.com/hebgc2009/index.php'
     const url = preUrl + '?a=eng-anc-list&cat_bid1=Z04&region=&title=&p=';
     const reqs = [request(url + '1'), request(url + '2')]
@@ -33,10 +43,10 @@ function gethebgc() {
             return returnValue
         }
     })
-    fetcher.fetchData()
+    return fetcher
 }
 function getccgp() {
-    const taskName='ccgp'
+    const taskName = 'ccgp'
     const preUrl = 'http://ccgp-hebei.gov.cn'
     const url = preUrl + '/zfcg/web/getBidingList_';
     const reqs = [request(url + '1.html'), request(url + '2.html')]
@@ -63,10 +73,10 @@ function getccgp() {
             return returnValue
         }
     })
-    fetcher.fetchData()
+    return fetcher
 }
 function getbidding() {
-    const taskName="hebeibidding"
+    const taskName = "hebeibidding"
     const preUrl = 'http://hebeibidding.com.cn/'
     const url = preUrl + '/tender/more.aspx'
 
@@ -107,8 +117,56 @@ function getbidding() {
             return returnValue
         }
     })
-    fetcher.fetchData()
+    return fetcher
 }
-getbidding()
-getccgp()
-gethebgc()
+
+function delayFetch() {
+    const currentTime = new Date()
+    if (currentTime.getHours() == config.stopHour) {
+        //下班时间，延迟到明天上班再抓取
+        const timeout = this.getNextFetchTimeout()
+        console.log(timeout + '分钟后将进行下次抓取')
+        setTimeout(fetchData, timeout * 60 * 1000)
+    } else {
+        console.log(config.timeout + '分钟后将进行下次抓取')
+        setTimeout(fetchData, config.timeout * 60 * 1000)
+    }
+}
+
+function getNextFetchTimeout() {
+    const hour = (24 - config.stopHour) + config.startHour
+    return hour * 60
+}
+
+function fetchData() {
+    const req = [biddingFetcher.fetchData(), ccgpFetcher.fetchData(), hebgcFetcher.fetchData()]
+
+    Promise.all(req).then(data=> {
+        const newData = data[0].concat(data[1]).concat(data[2])
+        if(newData.length>0){
+            const jsonData=[{
+                name:'河北招标投标网 bidding',
+                data:data[0]
+            },{
+                name:'河北政府采购网 ccgp',
+                data:data[1]
+            },{
+                name:'河北建设工程信息网 hebgc',
+                data:data[2]
+            }]
+            fs.writeFileSync('newData.json',JSON.stringify(jsonData,null,4),'utf-8')
+            return sendMessage(newData)
+        }
+        return 'noNewData'
+
+    }).then(()=> {
+        delayFetch()
+    }).catch(()=> {
+        console.log('发生错误')
+        delayFetch()
+    })
+}
+const biddingFetcher = getbidding()
+const ccgpFetcher = getccgp()
+const hebgcFetcher = gethebgc()
+fetchData()
